2c7d1f114c232209cf3fbb5ff9b0056f920438d0
Riosmena <emi.riosmena@gmail.com>
2023-10-24 14:01:32 -0600

    Created About Section

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,9 +1,11 @@
+import AboutSection from "../Components/About/AboutSection/AboutSection"
 import HeroSection from "../Components/HeroSection/HeroSection"
 
 const Home = () => {
     return (
         <div>
             <HeroSection />
+            <AboutSection />
         </div>
     )
 }
